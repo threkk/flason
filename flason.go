31bb2c0bdd3ec0f88408e25b6dbf36510021d3d5
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2019-10-31 21:31:07 +0100

    fix: Fixes recursion issue with the slices.

--- flason.go
+++ flason.go
@@ -16,6 +16,7 @@ func FlattenJson(str string) (pairs []JsonPair, err error) {
 	// We recover in case we panic during execution.
 	defer func() {
 		if r := recover(); r != nil {
+			// TODO: Add debugging flag with the value.
 			err = errors.New("Unknown value type during recursion.")
 			pairs = nil
 		}
@@ -34,47 +35,51 @@ func FlattenJson(str string) (pairs []JsonPair, err error) {
 		for k, v := range curr {
 			switch v.(type) {
 			case string:
-				path := strings.Join([]string{prev, ".", k}, "")
+				path := strings.Join([]string{prev, k}, "")
 				pairs = append(pairs, JsonPair{
 					Path:  path,
 					Value: v.(string),
 				})
 			case float64:
-				path := strings.Join([]string{prev, ".", k}, "")
+				path := strings.Join([]string{prev, k}, "")
 				value := strconv.FormatFloat(v.(float64), 'g', -1, 64)
 				pairs = append(pairs, JsonPair{
 					Path:  path,
 					Value: value,
 				})
 			case bool:
-				path := strings.Join([]string{prev, ".", k}, "")
+				path := strings.Join([]string{prev, k}, "")
 				value := strconv.FormatBool(v.(bool))
 				pairs = append(pairs, JsonPair{
 					Path:  path,
 					Value: value,
 				})
 			case nil:
-				path := strings.Join([]string{prev, ".", k}, "")
+				path := strings.Join([]string{prev, k}, "")
 				pairs = append(pairs, JsonPair{
 					Path:  path,
 					Value: "null",
 				})
 			case []interface{}:
+				path := strings.Join([]string{prev, k}, "")
+
+				sliceToMap := make(map[string]interface{})
 				for index, w := range v.([]interface{}) {
-					path := strings.Join([]string{prev, "[", strconv.Itoa(index), "]"}, "")
-					reduce(path, w.(map[string]interface{}))
+					key := strings.Join([]string{"[", strconv.Itoa(index), "]"}, "")
+					sliceToMap[key] = w
 				}
+
+				reduce(path, sliceToMap)
 			case map[string]interface{}:
-				for newKey, w := range v.(map[string]interface{}) {
-					path := strings.Join([]string{prev, ".", newKey}, "")
-					reduce(path, w.(map[string]interface{}))
-				}
+				path := strings.Join([]string{prev, k, "."}, "")
+				reduce(path, v.(map[string]interface{}))
+
 			default:
-				panic("Unknown value type")
+				panic(v)
 			}
 		}
 	}
 
-	reduce("", data)
+	reduce(".", data)
 	return pairs, nil
 }
