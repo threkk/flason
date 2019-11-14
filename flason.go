231f17900c2acf42f913123846b45d3250f930d5
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2019-11-15 00:56:05 +0100

    feat: MVP of flason library.
    - Parse all the JSON based on the specification:
        * Single values.
        * Slices.
        * Objects.
        * Recursion.
    - Returns the pairs ordered.
    - Optional starting character.

--- flason.go
+++ flason.go
@@ -3,6 +3,7 @@ package flason
 import (
 	"encoding/json"
 	"errors"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -12,7 +13,21 @@ type JsonPair struct {
 	Value string
 }
 
-func FlattenJson(str string) (pairs []JsonPair, err error) {
+type byPath []JsonPair
+
+func (p byPath) Len() int {
+	return len(p)
+}
+
+func (p byPath) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p byPath) Less(i, j int) bool {
+	return p[i].Path < p[j].Path
+}
+
+func FlattenJson(str, starter string) (pairs []JsonPair, err error) {
 	// We recover in case we panic during execution.
 	defer func() {
 		if r := recover(); r != nil {
@@ -22,64 +37,59 @@ func FlattenJson(str string) (pairs []JsonPair, err error) {
 		}
 	}()
 
-	var data map[string]interface{}
+	var data interface{}
 	if err := json.Unmarshal([]byte(str), &data); err != nil {
 		return nil, err
 	}
 
 	// TODO: Make it asynchronous using go routines.
-	var reduce func(prev string, curr map[string]interface{})
-	reduce = func(prev string, curr map[string]interface{}) {
+	var reduce func(prev string, curr interface{})
+	reduce = func(prev string, curr interface{}) {
 
-		// Recover from the panic and return nicely.
-		for k, v := range curr {
-			switch v.(type) {
-			case string:
-				path := strings.Join([]string{prev, k}, "")
-				pairs = append(pairs, JsonPair{
-					Path:  path,
-					Value: v.(string),
-				})
-			case float64:
-				path := strings.Join([]string{prev, k}, "")
-				value := strconv.FormatFloat(v.(float64), 'g', -1, 64)
-				pairs = append(pairs, JsonPair{
-					Path:  path,
-					Value: value,
-				})
-			case bool:
-				path := strings.Join([]string{prev, k}, "")
-				value := strconv.FormatBool(v.(bool))
-				pairs = append(pairs, JsonPair{
-					Path:  path,
-					Value: value,
-				})
-			case nil:
-				path := strings.Join([]string{prev, k}, "")
-				pairs = append(pairs, JsonPair{
-					Path:  path,
-					Value: "null",
-				})
-			case []interface{}:
-				path := strings.Join([]string{prev, k}, "")
-
-				sliceToMap := make(map[string]interface{})
-				for index, w := range v.([]interface{}) {
-					key := strings.Join([]string{"[", strconv.Itoa(index), "]"}, "")
-					sliceToMap[key] = w
-				}
-
-				reduce(path, sliceToMap)
-			case map[string]interface{}:
-				path := strings.Join([]string{prev, k, "."}, "")
-				reduce(path, v.(map[string]interface{}))
-
-			default:
-				panic(v)
+		// These cover all the possible JSON types.
+		// https://golang.org/pkg/encoding/json/#Unmarshal
+		switch curr.(type) {
+		case string:
+			pairs = append(pairs, JsonPair{
+				Path:  prev,
+				Value: curr.(string),
+			})
+		case float64:
+			value := strconv.FormatFloat(curr.(float64), 'g', -1, 64)
+			pairs = append(pairs, JsonPair{
+				Path:  prev,
+				Value: value,
+			})
+		case bool:
+			value := strconv.FormatBool(curr.(bool))
+			pairs = append(pairs, JsonPair{
+				Path:  prev,
+				Value: value,
+			})
+		case nil:
+			pairs = append(pairs, JsonPair{
+				Path:  prev,
+				Value: "null",
+			})
+		case []interface{}:
+			for index, value := range curr.([]interface{}) {
+				path := strings.Join([]string{prev, "[", strconv.Itoa(index), "]"}, "")
+				reduce(path, value)
 			}
+
+		case map[string]interface{}:
+			for key, value := range curr.(map[string]interface{}) {
+				path := strings.Join([]string{prev, ".", key}, "")
+				reduce(path, value)
+			}
+
+		default:
+			panic(curr)
 		}
 	}
 
-	reduce(".", data)
+	reduce(starter, data)
+	sort.Sort(byPath(pairs))
+
 	return pairs, nil
 }
--- flason_test.go
+++ flason_test.go
@@ -1,14 +1,32 @@
 package flason
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestFlattenJson(t *testing.T) {
+func eq(a, b []JsonPair) bool {
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for index := range a {
+		if a[index].Path != b[index].Path || a[index].Value != b[index].Value {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestBasicTypes(t *testing.T) {
 	type args struct {
 		str string
 	}
+
 	tests := []struct {
 		name    string
 		args    args
@@ -22,6 +40,19 @@ func TestFlattenJson(t *testing.T) {
 			},
 			want:    nil,
 			wantErr: true,
+		},
+		{
+			name: "Single value",
+			args: args{
+				str: `"a"`,
+			},
+			want: []JsonPair{
+				JsonPair{
+					Path:  "",
+					Value: "a",
+				},
+			},
+			wantErr: false,
 		},
 		{
 			name: "Basic string",
@@ -49,16 +80,84 @@ func TestFlattenJson(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "Basic null",
+			args: args{
+				str: `{ "key": null }`,
+			},
+			want: []JsonPair{
+				JsonPair{
+					Path:  ".key",
+					Value: "null",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Basic boolean",
+			args: args{
+				str: `{ "key": true, "otherKey": false }`,
+			},
+			want: []JsonPair{
+				JsonPair{
+					Path:  ".key",
+					Value: "true",
+				},
+				JsonPair{
+					Path:  ".otherKey",
+					Value: "false",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Basic array",
+			args: args{
+				str: `[ "a", 1, true ]`,
+			},
+			want: []JsonPair{
+				JsonPair{
+					Path:  "[0]",
+					Value: "a",
+				},
+				JsonPair{
+					Path:  "[1]",
+					Value: "1",
+				},
+				JsonPair{
+					Path:  "[2]",
+					Value: "true",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Basic object",
+			args: args{
+				str: `{ "inv": { "true": true, "false": false } }`,
+			},
+			want: []JsonPair{
+				JsonPair{
+					Path:  ".inv.false",
+					Value: "false",
+				},
+				JsonPair{
+					Path:  ".inv.true",
+					Value: "true",
+				},
+			},
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := FlattenJson(tt.args.str)
+			got, err := FlattenJson(tt.args.str, "")
 			if (err != nil) != tt.wantErr {
 				t.Errorf("FlattenJson() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
+			if !eq(got, tt.want) {
 				t.Errorf("FlattenJson() = %v, want %v", got, tt.want)
 			}
 		})
